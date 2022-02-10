ddb10e6962630f827953fb0dce5345a4b39c4978
Rushi <189489rishi@gmail.com>
2022-02-10 20:20:20 +0530

    Added: Employee present or absent

--- Program.cs
+++ Program.cs
@@ -7,7 +7,16 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" Employee Wage ");
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+            if (empcheck == 0)
+            {
+                Console.WriteLine("employee is present");
+            }
+            else
+            {
+                Console.WriteLine("employee is absent");
+            }
 
         }
     }
